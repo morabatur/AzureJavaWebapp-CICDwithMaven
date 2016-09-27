e497006b0509e295f23ab5e156170dd6e4301555
tr22user <tr22user@outlook.com>
2016-09-27 16:17:30 -0500

    Quotes

--- deploy.cmd
+++ deploy.cmd
@@ -1,3 +1,3 @@
-set MAVEN_OPTS=-Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false
-set JAVA_OPTS=-Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false
+set MAVEN_OPTS="-Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false"
+set JAVA_OPTS="-Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false"
 .\apache-maven-3.3.9\bin\mvn package -Ddir=D:\home\site\wwwroot\webapps

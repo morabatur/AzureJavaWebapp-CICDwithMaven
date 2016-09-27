df264580fd91632a31a1669155034d21554c340b
tr22user <tr22user@outlook.com>
2016-09-27 16:29:56 -0500

    Removed other env vars

--- deploy.cmd
+++ deploy.cmd
@@ -1,4 +1,2 @@
-set MAVEN_OPTS="-Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false"
-set JAVA_OPTS="-Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false"
-set JAVA_HOME="D:\Progra~1\Java\jdk1.7.0_71"
+set MAVEN_OPTS=-Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false
 .\apache-maven-3.3.9\bin\mvn package -Ddir=D:\home\site\wwwroot\webapps

5dfb6ac87ccde7232bdbbbabeb4c2656e9210906
tr22user <tr22user@outlook.com>
2016-09-27 16:24:53 -0500

    Added java home

--- deploy.cmd
+++ deploy.cmd
@@ -1,3 +1,4 @@
 set MAVEN_OPTS="-Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false"
 set JAVA_OPTS="-Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false"
-.\apache-maven-3.3.9\bin\mvn package -Ddir=D:\home\site\wwwroot\webapps -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false
+set JAVA_HOME="D:\Progra~1\Java\zulu8.15.0.1-jdk8.0.92-win_x64"
+.\apache-maven-3.3.9\bin\mvn package -Ddir=D:\home\site\wwwroot\webapps

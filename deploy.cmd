58a13c94c1e61f32d170c3b3d712a10a115b4ec1
tr22user <tr22user@outlook.com>
2016-09-27 14:34:04 -0500

    Added MVN_OPTS

--- deploy.cmd
+++ deploy.cmd
@@ -1,3 +1,3 @@
-set M2_HOME=.\apache-maven-3.3.9
-set JVM_OPTS=" -Djava.net.preferIPv4Stack=true"
+set MAVEN_OPTS=-Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false
 .\apache-maven-3.3.9\bin\mvn package
+xcopy .\target\*.war D:\home\site\wwwroot\

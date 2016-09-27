d803a714245c35675c4dc3662a9da2b5b4c5b552
tr22user <tr22user@outlook.com>
2016-09-27 16:11:41 -0500

    Removed quotes

--- deploy.cmd
+++ deploy.cmd
@@ -1,3 +1,3 @@
 set MAVEN_OPTS=-Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false
-set JAVA_HOME="D:\Progra~1\Java\jdk1.8.0_73"
+set JAVA_HOME=D:\Progra~1\Java\jdk1.8.0_73
 .\apache-maven-3.3.9\bin\mvn package -Ddir="D:\home\site\wwwroot\webapps"

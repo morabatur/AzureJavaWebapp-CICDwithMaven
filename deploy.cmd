7e1263a78bbaf6ba5c928dff914b2fb43e2be051
tr22user <tr22user@outlook.com>
2016-09-27 16:26:34 -0500

    Modified java home

--- deploy.cmd
+++ deploy.cmd
@@ -1,4 +1,4 @@
 set MAVEN_OPTS="-Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false"
 set JAVA_OPTS="-Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false"
-set JAVA_HOME="D:\Progra~1\Java\zulu8.15.0.1-jdk8.0.92-win_x64"
+set JAVA_HOME="D:\Progra~1\Java\jdk1.7.0_71"
 .\apache-maven-3.3.9\bin\mvn package -Ddir=D:\home\site\wwwroot\webapps
